62836b3950da2eea7a109c038ff802500f2b7f56
asyks <aaron@aaronsykes.com>
2020-12-28 20:40:46 -0800

    Handle add/remove filter colors

--- src/features/filtersSlice.js
+++ src/features/filtersSlice.js
@@ -7,9 +7,25 @@ export default function filtersReducer(state = initialState, action) {
   switch (action.type) {
     case 'filters/statusFilterChanged': {
       return {
-        ...state.filters,
+        ...state,
         status: action.payload
       }
+    }
+    case 'filters/addColorFilter': {
+      return {
+        ...state,
+        'colors': [
+          ...state.colors,
+          action.payload
+        ]
+      }
+    }
+    case 'filters/removeColorFilter': {
+      state.colors.filter(color => {
+        if (color != action.payload) {
+          return color
+        }
+      })
     }
     default:
       return state
