17b47c3fb0484dfbe76fdba14be6c6c10e5cf16a
asyks <aaron@aaronsykes.com>
2020-12-30 20:21:15 -0800

    Add root reducer (mistakenly excluded from previous commit)

--- src/reducer.js
+++ src/reducer.js
@@ -3,13 +3,6 @@ import { combineReducers } from 'redux'
 import filtersReducer from './features/filtersSlice'
 import todosReducer from './features/todosSlice'
 
-export default function rootReducer(state = {}, action) {
-  return {
-    todos: todosReducer(state.todos, action),
-    filters: filtersReducer(state.filters, action)
-  }
-}
-
 const rootReducer = combineReducers({
   todos: todosReducer,
   filters: filtersReducer,
