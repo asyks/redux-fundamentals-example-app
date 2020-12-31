da60d6749b8501a2721c665b418c87988a7c19c6
asyks <aaron@aaronsykes.com>
2020-12-30 21:15:43 -0800

    Log some fake activity on index load

--- src/index.js
+++ src/index.js
@@ -1,13 +1,26 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import './index.css'
-import App from './App'
+import store from './store'
 
-import './api/server'
+// Log the initial state
+console.log('initial.state : ', store.getState())
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-)
+const unsubscribe = store.subscribe(() => {
+  console.log('State after dispatch: ', store.getState())
+})
+
+store.dispatch({type: 'todos/todoAdded', payload: 'Learn about actions' })
+store.dispatch({ type: 'todos/todoAdded', payload: 'Learn about reducers' })
+store.dispatch({ type: 'todos/todoAdded', payload: 'Learn about stores' })
+
+store.dispatch({ type: 'todos/todoToggled', payload: 0 })
+store.dispatch({ type: 'todos/todoToggled', payload: 1 })
+
+store.dispatch({ type: 'filters/statusFilterChanged', payload: 'Active' })
+
+store.dispatch({
+  type: 'filters/colorFilterChanged',
+  payload: {color: 'red', changeType: 'selected'}
+})
+
+unsubscribe()
+
+store.dispatch({ type: 'todos/todoAdded', payload: 'Try creating a store' })
