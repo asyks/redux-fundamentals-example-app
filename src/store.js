da557877509ba9fd019dc7ddd85ba699cb5d1eb2
asyks <aaron@aaronsykes.com>
2021-01-03 20:26:27 -0800

    Use composeWithDevTools to enable redux devtools in browser

--- src/store.js
+++ src/store.js
@@ -1,7 +1,11 @@
 import { createStore, applyMiddleware } from 'redux'
 import rootReducer from './reducer'
+import { composeWithDevTools } from 'redux-devtools-extension'
 import { delayedMessageMiddleware } from './exampleAddons/middleware'
 
-const middlewareEnhancer = applyMiddleware(delayedMessageMiddleware)
-const store = createStore(rootReducer, undefined, middlewareEnhancer)
+const composedEnhancer = composeWithDevTools (
+  applyMiddleware(delayedMessageMiddleware)
+)
+
+const store = createStore(rootReducer, undefined, composedEnhancer)
 export default store
