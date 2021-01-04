79d6753abb6b9cb46f1833fd8cfd8c320607c159
asyks <aaron@aaronsykes.com>
2021-01-03 19:21:05 -0800

    Add action logging middleware and use it in the store

--- src/store.js
+++ src/store.js
@@ -1,8 +1,7 @@
 import { createStore, applyMiddleware } from 'redux'
 import rootReducer from './reducer'
-import { print1, print2, print3 } from './exampleAddons/middleware'
+import { loggerMiddleware } from './exampleAddons/middleware'
 
-const middlewareEnhancer = applyMiddleware(print1, print2, print3)
+const middlewareEnhancer = applyMiddleware(loggerMiddleware)
 const store = createStore(rootReducer, undefined, middlewareEnhancer)
-
 export default store
