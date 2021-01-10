acc8fe54e1c718c77773cf1d8b398f28c5449745
asyks <aaron@aaronsykes.com>
2021-01-09 20:26:58 -0800

    Implement add todo item

--- src/features/header/Header.js
+++ src/features/header/Header.js
@@ -1,9 +1,20 @@
 import React, { useState } from 'react'
+import { useDispatch } from 'react-redux'
 
 const Header = () => {
   const [text, setText] = useState('')
+  const dispatch = useDispatch()
 
   const handleChange = (e) => setText(e.target.value)
+
+  const handleKeyDown = (e) => {
+    const trimmedText = e.target.value.trim()
+    // If the user pressed the Enter key:
+    if (e.which === 13 && trimmedText) {
+      dispatch({type: 'todos/todoAdded', payload: trimmedText})
+      setText('')
+    }
+  }
 
   return (
     <header className="header">
@@ -12,6 +23,7 @@ const Header = () => {
         placeholder="What needs to be done?"
         value={text}
         onChange={handleChange}
+        onKeyDown={handleKeyDown}
       />
     </header>
   )
--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import { Provider } from 'react-redux'
+
 import './index.css'
 import App from './App'
 
@@ -9,7 +11,9 @@ import store from './store'
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 )
