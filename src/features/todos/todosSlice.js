0c35334e390ef2ea74407136fe85aeabd4c5e698
asyks <aaron@aaronsykes.com>
2021-01-19 20:43:33 -0800

    Remove deprecated function `nextTodoId`

--- src/features/todos/todosSlice.js
+++ src/features/todos/todosSlice.js
@@ -2,11 +2,6 @@ import { client } from "../../api/client"
 
 const initialState = []
 
-function nextTodoId(todos = []) {
-  const maxId = todos.reduce((maxId, todo) => Math.max(todo.id, maxId), -1)
-  return maxId + 1
-}
-
 export default function todosReducer(state = initialState, action) {
   switch (action.type) {
     case 'todos/todoAdded': {
