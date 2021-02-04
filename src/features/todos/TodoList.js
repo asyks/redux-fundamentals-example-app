4844b0465afdfda963eb544ad8a1ef8d395ce56f
asyks <aaron@aaronsykes.com>
2021-02-03 20:58:11 -0800

    Add api GET todos delay, and display loading spinner

--- src/features/todos/TodoList.js
+++ src/features/todos/TodoList.js
@@ -6,6 +6,15 @@ import { selectFilteredTodoIds } from './todosSlice'
 
 const TodoList = () => {
   const todos = useSelector(selectFilteredTodoIds)
+  const loadingStatus = useSelector(state => state.todos.status)
+
+  if (loadingStatus === 'loading') {
+    return (
+      <div className="todo-list">
+        <div className="loader"/>
+      </div>
+    )
+  }
 
   const renderedListItems = todos.map((todoId) => {
     return <TodoListItem key={todoId} id={todoId} />
